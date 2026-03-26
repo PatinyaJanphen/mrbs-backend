d89abb0bed0dc7db3fcec9fd1a11b4212df4b5cf
fifa <den372115@gmail.com>
2026-03-26 12:10:33 +0700

    adds google login

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+use App\Http\Controllers\Auth\GoogleAuthController;
+
+Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect']);
+Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
+
 // Company Routes
 Route::get('/companies', [CompanyController::class, 'index']);
 Route::post('/companies', [CompanyController::class, 'store']);
