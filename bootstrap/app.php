12f4fce1d0697b0b6d4aa3e9d775b8fc896df425
fifa <den372115@gmail.com>
2026-06-10 17:01:35 +0700

    feat: initialize application configuration with custom middleware and stateful API settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'active.user' => EnsureUserIsActive::class,
         ]);
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            'api/auth/google/callback',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
