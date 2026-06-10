628cda3aa0be6a715dd551692e098be3b90c5c1a
fifa <den372115@gmail.com>
2026-06-10 17:29:09 +0700

    feat: initialize application configuration and register middleware for user status and CSRF exclusion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,8 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-
         $middleware->validateCsrfTokens(except: [
             'api/auth/google/callback',
         ]);
